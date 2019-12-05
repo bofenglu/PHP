fa7e558c6042e27c6c8efae2ab99c627689de31e
bofenglu <bofenglu@gmail.com>
2019-12-05 21:40:04 +0800

    32

---
+++ ch6/test.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>PHP test1</title>
+</head>
+<body>
+    <?php
+        printf "Hello World";
+    ?>
+    
+</body>
+</html>
